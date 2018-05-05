5c8108ebaa3258562524f837e469445039e6324a
Yukinim <yukinim86@gmail.com>
2018-05-05 19:47:57 +0900

    1

--- src/main/java/com/my/controller/KeywordController.java
+++ src/main/java/com/my/controller/KeywordController.java
@@ -23,6 +23,7 @@ public class KeywordController {
 	 @RequestMapping(value="/selectKeywords",method=RequestMethod.GET)
 	    public @ResponseBody Map<String,Object> selectKeywords(HttpServletRequest request) throws Exception{
 	    	List<Map<String,Object>> resultList = mapper.selectKeywords();
+	    	resultList = null;
 	        Map<String, Object> jsonObject = new HashMap<String, Object>();
 	        jsonObject.put("resultList", resultList);
 	        return jsonObject;
